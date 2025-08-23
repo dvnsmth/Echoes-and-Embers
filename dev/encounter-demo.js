5a0999efde0d07082b0b8c0065bd7569fcc03d99
davinjos <davinjos@gmail.com>
2025-08-22 22:49:07 -0700

    gogogo

---
+++ dev/encounter-demo.js
@@ -0,0 +1,17 @@
+import { rollPreset } from "../encounterPresets.js";
+import { buildEncounterFromTable } from "../spawnTables.js";
+import { instantiateEncounter, collapseEncounter, xpForEncounter } from "../encounters.js";
+
+window.debugRollPreset = (preset="forest_road_t1", lvl=2, size=3, seed="demo") => {
+  const defs = rollPreset(preset, { partyLevel: lvl, partySize: size, seed });
+  console.table(collapseEncounter(defs));
+  console.log("XP:", xpForEncounter(defs, lvl));
+  return instantiateEncounter(defs);
+};
+
+window.debugSpawnTable = (table="crypt_undead_t2", lvl=4, size=3, seed="t2") => {
+  const defs = buildEncounterFromTable(table, { partyLevel: lvl, partySize: size, seed });
+  console.table(collapseEncounter(defs));
+  console.log("XP:", xpForEncounter(defs, lvl));
+  return instantiateEncounter(defs);
+};
